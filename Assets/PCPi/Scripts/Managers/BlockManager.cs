cb9cd174b7cbbac0966f638ae7916886dc415316
GChisenhall <gchisenhall33@gmail.com>
2019-05-02 15:43:29 -0400

    Intial Commit
    Block Builder for ProtoBlocks

---
+++ Assets/PCPi/Scripts/Managers/BlockManager.cs
@@ -0,0 +1,51 @@
+﻿/// <summary>
+/// Project: ProtoBlock Builder
+/// Made for use in Projects: Unicorn Snake & Protoblocks (...more to come?)
+/// Filename: BlockManager.cs
+/// Created by PCPi & AltX
+/// Written by: Gary Chisenhall
+/// </summary>
+#region /// USING
+using UnityEngine;
+#endregion
+namespace PCPi.scripts.Managers
+{
+    [AddComponentMenu("PCPi/ProtoBlock Scene Manager")]
+    public class BlockManager : MonoBehaviour
+    {
+        #region /// Properties
+        private GameObject selectedBlock;
+        
+        private GameObject baseBlock;
+        public GameObject SelectedBlock { get => selectedBlock; set => selectedBlock = value; }
+        public GameObject BaseBlock { get => baseBlock; set => baseBlock = value; }
+        #endregion
+
+        #region /// Property Methods
+        public GameObject GetBaseBlock()
+        {
+            return BaseBlock;
+        }
+
+        public void SetBaseBlock(GameObject value)
+        {
+            BaseBlock = value;
+            //PlaceBase(value);
+        }
+
+        public GameObject GetSelectedBlock()
+        {
+            return SelectedBlock;
+        }
+
+        public void SetSelectedBlock(GameObject value)
+        {
+            SelectedBlock = value;
+        }
+        #endregion
+        //public static void PlaceBase(GameObject baseBlock)
+        //{
+        //    BlockSpawnManager.PlaceBaseBlock(baseBlock);
+        //}
+    }
+}
---
+++ Assets/PCPi/Scripts/Managers/BlockSettings.cs
@@ -0,0 +1,67 @@
+﻿/// <summary>
+/// Project: ProtoBlock Builder
+/// Made for use in Projects: Unicorn Snake & Protoblocks (...more to come?)
+/// Filename: BlockSettings.cs
+/// Created by PCPi & AltX
+/// Written by: Gary Chisenhall & Topher Lee
+/// </summary>
+#region /// USING
+using UnityEngine;
+#endregion
+namespace PCPi.scripts.Managers
+{
+    /// <summary>
+    /// BlockSettings Class for ProtoBlock Builder
+    /// </summary>
+    public class BlockSettings : MonoBehaviour
+    {
+        #region /// Constants
+        private const string MANAGER_PATH = "/PCPi/Prefabs/Managers/ProtoBlockSceneManager.prefab";
+        #endregion
+        #region /// Local Variables
+        private GameObject BlockManager;
+        private GameObject BlockToSpawn;
+        private Color blockColor;
+        private GameObject blockSurface;
+        #endregion
+        #region /// Global Variables
+        public Material defaultMaterial;
+        public Material highlightMaterial;
+        #endregion
+        void Start()
+        {
+            BlockManager = Resources.Load<GameObject>(MANAGER_PATH);
+            if (!BlockManager)
+            {
+                Instantiate(BlockManager);
+            }
+            else
+            {
+                /// Do nothing
+            }
+            defaultMaterial = gameObject.GetComponent<Renderer>().material;
+        }
+        private void OnMouseDown()
+        {
+            BlockSpawnManager.PlaceSelectedBlock(BlockToSpawn, transform.position + transform.position, transform);
+        }
+        /// <summary>
+        /// Activates highlighter material & assigns block to spawn
+        /// </summary>
+        private void OnMouseEnter()
+        {
+            if (BlockManager)
+            {
+                BlockToSpawn = BlockManager.GetComponent<BlockManager>().GetSelectedBlock();
+            }
+            gameObject.GetComponent<Renderer>().material = highlightMaterial;
+        }
+        /// <summary>
+        /// Deactivates highlighter material
+        /// </summary>
+        private void OnMouseExit()
+        {
+            gameObject.GetComponent<Renderer>().material = defaultMaterial;
+        }
+    }
+}
